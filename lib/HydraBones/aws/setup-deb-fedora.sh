dd762b13dbf693ec0622a9e05110fa018eb9e6e2
Colin Gross <grosscol@umich.edu>
2014-11-03 10:09:43 -0500

    Update script to accomodate multiple executions.

--- lib/HydraBones/aws/setup-deb-fedora.sh
+++ lib/HydraBones/aws/setup-deb-fedora.sh
@@ -2,7 +2,9 @@
 
 # Obtain required packages
 sudo apt-get update
-sudo apt-get install -y -R curl openjdk-7-jre openjdk-7-jdk tomcat8 tomcat8-admin git
+sudo apt-get install -y --no-install-recommends curl less openjdk-7-jre tomcat8 tomcat8-admin git
+# If you were going to build fedora4 also get the openjdk-jdk
+# sudo apt-get install -y --no-install-recommends openjdk-7-jdk 
 
 # Configure environment variables, and append to bash.rc
 
@@ -10,6 +12,7 @@ sudo apt-get install -y -R curl openjdk-7-jre openjdk-7-jdk tomcat8 tomcat8-admi
 if env | grep -q '^CATALINA_HOME=' > /dev/null
 then
   # Var already exported
+  echo "CATLINA_HOME already exported: $CATALINA_HOME"
 else
   export CATALINA_HOME
   echo "export CATALINA_HOME=/usr/share/tomcat8" >> ~/.bashrc
@@ -19,16 +22,29 @@ fi
 if env | grep -q '^JAVA_HOME=' > /dev/null
 then
   # Var already exported
+  echo "JAVA_HOME already exported: $JAVA_HOME"
 else
   export JAVA_HOME
   echo "export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64" >> ~/.bashrc
 fi
 
 # just get the WAR files.
-mkdir sudo /usr/local/fcrepo
+sudo mkdir -p /usr/local/fcrepo
 cd /usr/local/fcrepo
-wget http://repo1.maven.org/maven2/org/fcrepo/fcrepo-webapp/4.0.0-beta-04/fcrepo-webapp-4.0.0-beta-04-auth.war
-wget http://repo1.maven.org/maven2/org/fcrepo/fcrepo-webapp/4.0.0-beta-04/fcrepo-webapp-4.0.0-beta-04.war
+if [ -e /usr/local/fcrepo/fcrepo-webapp-4.0.0-beta-03-auth.war ]
+then
+  echo "fcrepo-webapp-4.0.0-beta-03-auth.war already present"
+else
+  wget http://repo1.maven.org/maven2/org/fcrepo/fcrepo-webapp/4.0.0-beta-03/fcrepo-webapp-4.0.0-beta-03-auth.war
+fi
+
+if [ -e /usr/local/fcrepo/fcrepo-webapp-4.0.0-beta-03.war ]
+then
+  echo "fcrepo-webapp-4.0.0-beta-03.war already present"
+else
+  wget http://repo1.maven.org/maven2/org/fcrepo/fcrepo-webapp/4.0.0-beta-03/fcrepo-webapp-4.0.0-beta-03.war
+fi
+
 
 # Install/Update Fedora 4 Repository
 # if [ -e /usr/local/fcrepo4/.git ]
@@ -47,12 +63,35 @@ sudo mkdir -p /usr/local/fedora-data
 sudo chown -R tomcat8:tomcat8 /usr/local/fedora-data
 
 # Copy the fedora war file to the default CATALINA_BASE directory
-sudo cp /usr/local/fcrepo/fcrepo-webapp-4.0.0-beta-04.war /var/lib/tomcat8/webapps/fcrepo
+if [ -e /var/lib/tomcat8/webapps/fcrepo.war ]
+then
+  echo "fcrepo-webapp-4.0.0-beta-03.war already deployed to /var/lib/tomcat8/webapps/fcrepo.war"
+else
+  sudo cp /usr/local/fcrepo/fcrepo-webapp-4.0.0-beta-03.war /var/lib/tomcat8/webapps/fcrepo.war
+fi
 
-# Modify the /etc/default/tomcat8 file so that the fedora home directory is writable by tomcat8
-# JAVA_OPTS="${JAVA_OPTS} -Dfcrepo.home=/usr/local/fedora-data" 
-# TODO: YOU ARE HERE:  As soon as you figure this part out, the bash script to bring up a new debian box should be good to go
+# Modify permissions file so that the web app directory is writable by tomcat8
+sudo chown -R tomcat8:tomcat8 /var/lib/tomcat8/webapps/fcrepo
+sudo chmod -R 0774 /var/lib/tomcat8/webapps/fcrepo
 
+# Modify the /etc/default/tomcat8 to have JAVA_OPTS include the fcrepo.home.
+if [ `grep -c -e '-Dfcrepo.home' < /etc/default/tomcat8` -gt 0 ]
+then
+  echo "fcrepo.home option already appended to /etc/default/tomcat8"
+else
+  echo "\n# Add the fcrepo.home option.  Without this, fedora4 will not start." | sudo tee --append /etc/default/tomcat8
+  echo "JAVA_OPTS=\"\${JAVA_OPTS} -Dfcrepo.home=/usr/local/fedora-data\"" | sudo tee --append /etc/default/tomcat8
+fi
 
 # Restart the tomcat service
-# sudo service tomcat8 restart
+sudo service tomcat8 restart
+
+# Check that fedora4 is up and running.
+echo "Checking if fedora4 is up and running..."
+resp=`curl --write-out '%{http_code}\n' -s -o /dev/null http://localhost:8080/fcrepo/rest`
+if [ ${resp} -eq 200 ]
+then
+  echo "Response code: ${resp}. Fedora repo is up and running."
+else
+  echo "Response code: ${resp}."
+fi
