1eec54c98daa627b8a008ac479d96a78c7579924
Colin Gross <grosscol@umich.edu>
2015-01-03 18:30:56 -0500

    Append .sh to s3 addresses.

--- scripts/aws/back_usr.sh
+++ scripts/aws/back_usr.sh
@@ -7,5 +7,5 @@ echo "Welcome to the Back Host." | tee /home/admin/back.txt
 echo "Hostname: $(hostname)" | tee --append /home/admin/back.txt
 
 # Grab setup scripts for solr and fedora
-wget https://s3.amazonaws.com/grosscol-hydra-scripts/setup-deb-fedora -O /home/admin/fedora-setup.sh
-wget https://s3.amazonaws.com/grosscol-hydra-scripts/setup-deb-solr -O /home/admin/solr-setup.sh
+wget https://s3.amazonaws.com/grosscol-hydra-scripts/setup-deb-fedora.sh -O /home/admin/fedora-setup.sh
+wget https://s3.amazonaws.com/grosscol-hydra-scripts/setup-deb-solr.sh -O /home/admin/solr-setup.sh
