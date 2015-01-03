681abc564ba94776c85bfc68b5e8ec6188382520
Colin Gross <grosscol@umich.edu>
2015-01-03 15:27:17 -0500

    Remove sudo from solr and fedora setup scripts.

--- scripts/aws/setup-deb-fedora.sh
+++ scripts/aws/setup-deb-fedora.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # Obtain required packages
-sudo apt-get update
-sudo apt-get install -y --no-install-recommends curl less openjdk-7-jre tomcat8 tomcat8-admin git
+apt-get update
+apt-get install -y --no-install-recommends curl less openjdk-7-jre tomcat8 tomcat8-admin git
 # If you were going to build fedora4 also get the openjdk-jdk
-# sudo apt-get install -y --no-install-recommends openjdk-7-jdk 
+# apt-get install -y --no-install-recommends openjdk-7-jdk 
 
 # Configure environment variables, and append to bash.rc
 
@@ -29,7 +29,7 @@ else
 fi
 
 # Make directory to hold fcrepo wars
-sudo mkdir -p /usr/local/fcrepo
+mkdir -p /usr/local/fcrepo
 
 # Just get the WAR files.
 #FCWARA_LOC=http://repo1.maven.org/maven2/org/fcrepo/fcrepo-webapp/4.0.0-beta-03/fcrepo-webapp-4.0.0-beta-03-auth.war
@@ -56,41 +56,41 @@ fi
 # if [ -e /usr/local/fcrepo4/.git ]
 # then
 #   cd /usr/local/fcrepo4
-#   sudo git pull
-#   sudo mvn compile
+#   git pull
+#   mvn compile
 # else
 #   cd /usr/local
-#   sudo git clone https://github.com/fcrepo4/fcrepo4.git
-#   sudo mvn install
+#   git clone https://github.com/fcrepo4/fcrepo4.git
+#   mvn install
 # fi
 
 # Make directory for Fedora data on this instance
-sudo mkdir -p /usr/local/fedora-data
-sudo chown -R tomcat8:tomcat8 /usr/local/fedora-data
+mkdir -p /usr/local/fedora-data
+chown -R tomcat8:tomcat8 /usr/local/fedora-data
 
 # Copy the fedora war file to the default CATALINA_BASE directory
 if [ -e /var/lib/tomcat8/webapps/fcrepo.war ]
 then
   echo "fcrepo-webapp-4.0.0-beta-03.war already deployed to /var/lib/tomcat8/webapps/fcrepo.war"
 else
-  sudo cp /usr/local/fcrepo/fcrepo-webapp-4.0.0-beta-03.war /var/lib/tomcat8/webapps/fcrepo.war
+  cp /usr/local/fcrepo/fcrepo-webapp-4.0.0-beta-03.war /var/lib/tomcat8/webapps/fcrepo.war
 fi
 
 # Modify permissions file so that the web app directory is writable by tomcat8
-sudo chown -R tomcat8:tomcat8 /var/lib/tomcat8/webapps/fcrepo
-sudo chmod -R 0774 /var/lib/tomcat8/webapps/fcrepo
+chown -R tomcat8:tomcat8 /var/lib/tomcat8/webapps/fcrepo
+chmod -R 0774 /var/lib/tomcat8/webapps/fcrepo
 
 # Modify the /etc/default/tomcat8 to have JAVA_OPTS include the fcrepo.home.
 if [ `grep -c -e '-Dfcrepo.home' < /etc/default/tomcat8` -gt 0 ]
 then
   echo "fcrepo.home option already appended to /etc/default/tomcat8"
 else
-  echo "\n# Add the fcrepo.home option.  Without this, fedora4 will not start." | sudo tee --append /etc/default/tomcat8
-  echo "JAVA_OPTS=\"\${JAVA_OPTS} -Dfcrepo.home=/usr/local/fedora-data\"" | sudo tee --append /etc/default/tomcat8
+  echo "\n# Add the fcrepo.home option.  Without this, fedora4 will not start." | tee --append /etc/default/tomcat8
+  echo "JAVA_OPTS=\"\${JAVA_OPTS} -Dfcrepo.home=/usr/local/fedora-data\"" | tee --append /etc/default/tomcat8
 fi
 
 # Restart the tomcat service
-sudo service tomcat8 restart
+service tomcat8 restart
 
 # Check that fedora4 is up and running.
 echo "Checking if fedora4 is up and running..."
--- scripts/aws/setup-deb-solr.sh
+++ scripts/aws/setup-deb-solr.sh
@@ -6,12 +6,12 @@ SOLR_VER="solr-4.10.2"
 SOLR_TAR_LOC="https://s3.amazonaws.com/cag-fed-deb/${SOLR_VER}.tgz"
 
 # Obtain required packages
-sudo apt-get update
-sudo apt-get install -y --no-install-recommends
-sudo apt-get install -y --no-install-recommends curl less openjdk-7-jre tomcat8 tomcat8-admin git
+apt-get update
+apt-get install -y --no-install-recommends
+apt-get install -y --no-install-recommends curl less openjdk-7-jre tomcat8 tomcat8-admin git
 
 # just get the WAR files.
-sudo mkdir -p /usr/local/solr
+mkdir -p /usr/local/solr
 cd /usr/local/solr
 
 # Get Solr from one of the mirrors.  This location will need to be updated with local copy of solr
@@ -19,12 +19,12 @@ if [ -e /usr/local/solr/${SOLR_VER}.tgz ]
 then
   echo "${SOLR_VER}.tgz already present"
 else
-  sudo wget $SOLR_TAR_LOC
+  wget $SOLR_TAR_LOC
 fi
 # Check that Solr was obtained from the location
 if [ -e /usr/local/solr/${SOLR_VER}.tgz ]
 then
-  sudo tar -xzf ${SOLR_VER}.tgz --keep-newer-files
+  tar -xzf ${SOLR_VER}.tgz --keep-newer-files
 else
   echo "Unable to obtain solr from $SOLR_TAR_LOC"
   exit 1
@@ -35,7 +35,7 @@ if [ -e /var/lib/tomcat8/webapps/solr.war ]
 then
   echo "solr.war already deployed to /var/lib/tomcat8/webapps/solr.war"
 else
-  sudo cp /usr/local/solr/${SOLR_VER}/dist/${SOLR_VER}.war  /var/lib/tomcat8/webapps/solr.war
+  cp /usr/local/solr/${SOLR_VER}/dist/${SOLR_VER}.war  /var/lib/tomcat8/webapps/solr.war
 fi
 
 # Modify the /etc/default/tomcat8 to have JAVA_OPTS include the fcrepo.home.
@@ -44,8 +44,8 @@ if [ `grep -c -e '-Dsolr.solr.home' < /etc/default/tomcat8` -gt 0 ]
 then
   echo "solr.solr.home option already appended to /etc/default/tomcat8"
 else
-  echo "\n# Add the solr.solr.home option.  Without this, Solr will not start." | sudo tee --append /etc/default/tomcat8
-  echo "JAVA_OPTS=\"\${JAVA_OPTS} -Dsolr.solr.home=${SOLR_HOME}\"" | sudo tee --append /etc/default/tomcat8
+  echo "\n# Add the solr.solr.home option.  Without this, Solr will not start." | tee --append /etc/default/tomcat8
+  echo "JAVA_OPTS=\"\${JAVA_OPTS} -Dsolr.solr.home=${SOLR_HOME}\"" | tee --append /etc/default/tomcat8
 fi
 
 # Modify the /etc/default/tomcat8 to have JAVA_OPTS include the fcrepo.home.
@@ -54,27 +54,27 @@ if [ `grep -c -e '-Dsolr.data.dir' < /etc/default/tomcat8` -gt 0 ]
 then
   echo "solr.data.dir option already appended to /etc/default/tomcat8"
 else
-  echo "\n# Add the solr.data.dir option.  Without this, Solr will not start." | sudo tee --append /etc/default/tomcat8
-  echo "JAVA_OPTS=\"\${JAVA_OPTS} -Dsolr.data.dir=${SOLR_DATA}\"" | sudo tee --append /etc/default/tomcat8
+  echo "\n# Add the solr.data.dir option.  Without this, Solr will not start." | tee --append /etc/default/tomcat8
+  echo "JAVA_OPTS=\"\${JAVA_OPTS} -Dsolr.data.dir=${SOLR_DATA}\"" | tee --append /etc/default/tomcat8
 fi
 
 # Create solr home and data location
-sudo mkdir -p ${SOLR_DATA}
+mkdir -p ${SOLR_DATA}
 
 # Change ownership of solr dir to tomcat8
-sudo chown -R tomcat8:tomcat8 ${SOLR_HOME}
+chown -R tomcat8:tomcat8 ${SOLR_HOME}
 
 # Copy schemaless files from solr package over to SOLR_HOME
-sudo cp -r /usr/local/solr/${SOLR_VER}/example/example-schemaless/* /opt/solr/schemaless
+cp -r /usr/local/solr/${SOLR_VER}/example/example-schemaless/* /opt/solr/schemaless
 
 # Copy required library jars to tomcat
-sudo cp /usr/local/solr/${SOLR_VER}/example/lib/ext/* /usr/share/tomcat8/lib
+cp /usr/local/solr/${SOLR_VER}/example/lib/ext/* /usr/share/tomcat8/lib
 
 # Remove the log4j libraries,  because they prevent Fedora4 from working
-sudo rm /usr/share/tomcat8/lib/log4j*.jar
-sudo rm /sur/share/tomcat8/lib/slf4j-log4j*.jar
+rm /usr/share/tomcat8/lib/log4j*.jar
+rm /sur/share/tomcat8/lib/slf4j-log4j*.jar
 
 # Modify the solr.xml to know to use tomcats' port 8080
 
 # Restart the tomcat service
-# sudo service tomcat8 restart
+# service tomcat8 restart
