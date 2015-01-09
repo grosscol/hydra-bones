f3903c49fcff44a9d3b4a4db64b6da473324f7ad
Colin Gross <grosscol@umich.edu>
2015-01-09 17:52:58 -0500

    Use bash var for torquebox3 location. Update unzip command to include destination dir.

--- scripts/aws/setup-deb-torque3.sh
+++ scripts/aws/setup-deb-torque3.sh
@@ -1,22 +1,23 @@
 #!/bin/sh
 
 TOR3_TAR_LOC='https://s3.amazonaws.com/cag-tb/torquebox-dist-3.1.1-bin.zip'
+TOR3_DIR_LOC=/usr/local/torquebox3
 
 # Create dir to store torquebox binary
-mkdir -p /usr/local/torquebox3
+mkdir -p ${TOR3_DIR_LOC}
 
 # Retrieve torquebox file from s3 storage and decompress
-if [ -e /usr/local/torquebox3/torquebox3.tgz ]
+if [ -e ${TOR3_DIR_LOC}/torquebox3.tgz ]
 then
   echo "torquebox3.tgz already present"
 else
-  wget $TOR3_TAR_LOC -O /usr/local/torquebox3/torquebox3.tgz
+  wget $TOR3_TAR_LOC -O ${TOR3_DIR_LOC}/torquebox3.tgz
 fi
 
-if [ -e /usr/local/torquebox3/torquebox3.tgz ]
+if [ -e ${TOR3_DIR_LOC}/torquebox3.tgz ]
 then
   # Unzip becase the tb3 guys are funny and it's actually a zip file.
-  unzip -u /usr/local/torquebox3/torquebox3.tgz 
+  unzip -u ${TOR3_DIR_LOC}/torquebox3.tgz -d ${TOR3_DIR_LOC}
 else
   echo "Unable to obtain torquebox3 from ${TOR3_TAR_LOC}"
   exit 1
@@ -25,8 +26,8 @@ fi
 # Environment variables for TB3 for both root and admin
 # TB3 wants to have it's own copy of jruby.  So about that... no.
 tb3_profile_adds='
-export TORQUEBOX_HOME=/usr/local/torquebox3/torquebox-3.1.1
-export JBOSS_HOME=\${TORQUEBOX_HOME}/jboss
+export TORQUEBOX_HOME=${TOR3_DIR_LOC}/torquebox-3.1.1
+export JBOSS_HOME=${TORQUEBOX_HOME}/jboss
 export JRUBY_HOME=/usr/local/rbenv/versions/jruby-1.7.18'
 
 echo "$tb3_profile_adds" | tee --append /home/admin/.bashrc
