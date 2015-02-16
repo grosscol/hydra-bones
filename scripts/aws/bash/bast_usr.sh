2984a1bf25e241168e878880b0b4fc7f0308cfd4
Colin Gross <grosscol@umich.edu>
2015-02-16 16:10:47 -0500

    Fix stack parameter of cfn-init call.

--- scripts/aws/bash/bast_usr.sh
+++ scripts/aws/bash/bast_usr.sh
@@ -2,4 +2,4 @@
 echo -e "Welcome to Bastion Host" | tee /home/ec2-user/bast.txt
 
 # Call cloud formation init to read stack metadata attribute for the bastion host, and take appropriate action
-/opt/aws/bin/cfn-init --stack-name hydra --resource bastHost --region us-east-1
+/opt/aws/bin/cfn-init --stack hydra --resource bastHost --region us-east-1
